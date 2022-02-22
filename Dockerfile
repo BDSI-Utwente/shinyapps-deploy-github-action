b9c1f6a8a2b53eb228479a944693013ab37b2914
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-02-22 23:02:54 +0100

    basic deploy action

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM r-base:latest
+
+RUN apt-get update && apt-get install -y \
+    libssl-dev \
+    libcurl4-openssl-dev
+RUN install2.r rsconnect
+
+WORKDIR /usr/app
+COPY deploy.R .
+
+CMD ["Rscript", "deploy.R"]
---
+++ deploy.R
@@ -0,0 +1,58 @@
+library(rsconnect)
+
+defined <- function(name) {
+  !is.null(Sys.getenv(name)) && Sys.getenv(name) != ""
+}
+
+required <- function(name) {
+  if (!defined(name)) {
+    stop("input or environment variable '", name, "' not set")
+  }
+  Sys.getenv(name)
+}
+
+optional <- function(name) {
+  if (!defined(name)) {
+    return(NULL)
+  }
+  Sys.getenv(name)
+}
+
+# resolve app dir
+appDir <- ifelse(
+  defined("INPUT_APPDIR"),
+  required("INPUT_APPDIR"),
+  required("GITHUB_WORKSPACE")
+)
+
+# required inputs
+appName <- required("INPUT_APPNAME")
+accountName <- required("INPUT_ACCOUNTNAME")
+accountToken <- required("INPUT_ACCOUNTTOKEN")
+accountSecret <- required("IUNPUT_ACCOUNTSECRET")
+
+# optional inputs
+appFiles <- optional("INPUT_APPFILES")
+appFileManifest <- optional("INPUT_APPFILEMANIFEST")
+appTitle <- optional("INPUT_APPTITLE")
+logLevel <- optional("INPUT_LOGLEVEL")
+
+# process appFiles
+if (!is.null(appFiles)) {
+  appFiles <- unlist(strsplit(appFiles, ",", TRUE))
+}
+
+# set up account
+cat("checking account info...")
+rsconnect::setAccountInfo(accountName, accountToken, accountSecret)
+
+# deploy application
+cat("deploying application...")
+rsconnect::deployApp(
+  appDir = appDir,
+  appFiles = appFiles,
+  appFileManifest = appFileManifest,
+  appName = appName,
+  appTitle = appTitle,
+  account = accountName
+)
